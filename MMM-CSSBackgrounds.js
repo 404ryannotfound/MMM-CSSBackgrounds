95c582d1dc2afd6b30fd583c90d62f9f4bb37151
404ryannotfound <2672028+404ryannotfound@users.noreply.github.com>
2020-03-10 17:07:42 +0800

    Fix to default innerHTML
    Added proper fallback to particles.

--- MMM-CSSBackgrounds.js
+++ MMM-CSSBackgrounds.js
@@ -143,7 +143,7 @@ Module.register("MMM-CSSBackgrounds", {
 			BGOutput.innerHTML =  "<div class='blobpit'></div>";
 			} else {
 			//Default to particles
-			BGOutput.innerHTML =   "Default to particles<div class='animation-wrapper'><div class='particle particle-1'></div><div class='particle particle-2'></div><div class='particle particle-3'></div><div class='particle particle-4'></div></div>";
+			BGOutput.innerHTML =   "<div id='particle-container'><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div><div class='particle'></div></div>";
 		}
 
 		return BGOutput;
